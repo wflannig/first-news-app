9d230417df24e11c4881a64a31f4031dd6949022
Will <admin@admins-Mac-Pro-217.local>
2016-03-12 10:06:45 -0700

    Added lots of shtuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask
+from flask import abort
 from flask import render_template
 app = Flask(__name__)
 
@@ -16,6 +17,16 @@ def index():
 	template = "index.html"
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
+
+#Function for the details page
+@app.route("/<row_id>/")
+def detail(row_id):
+	template = "detail.html"
+	object_list = get_csv()
+	for row in object_list:
+		if row['id'] == row_id: 
+			return render_template(template, object=row)
+	abort(404)
 
 
 
